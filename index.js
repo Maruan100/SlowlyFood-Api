df798af18213c922894ebaef2e7d8acf7391589f
Maru <myousri@jabary.com>
2021-08-18 13:16:13 +0200

    fix: mongo url

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-'use strict'
+"use strict";
 
-
-const mongoose = require('mongoose');
-const app = require('./app');
+const mongoose = require("mongoose");
+const app = require("./app");
 const port = process.env.PORT || 5000;
 
-mongoose.set('useFindAndModify',false);
+mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://maru:1234@clusterprueba01-knt6b.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true})
-    .then(()  =>{
-        console.log('Conexion inciada correctamente!!');
+mongoose
+  .connect(
+    "mongodb+srv://1234:1234@slowlyfood.knt6b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("Conexion inciada correctamente!!");
 
-        //Crear servidor y escuchar peticiones http
-
-        app.listen(port,() => {
-            console.log('Servidor corriendo en http://localhost:'+port);
-
-        });
-
-});
+    app.listen(port, () => {
+      console.log("Servidor corriendo en http://localhost:" + port);
+    });
+  });
