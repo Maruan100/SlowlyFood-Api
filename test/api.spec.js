271dacd4ca23f7d27431baf4f9bf4085c2698190
Maruan100 <myousri@jabary.com>
2022-02-04 17:23:51 +0100

    feat: add test to app

---
+++ test/api.spec.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const app = require('../src/app.js');
+
+
+describe("GET /articles", () => {
+  it("should return articles list", (done) => {
+    request(app)
+      .get("/api/articles")
+      .expect(200)
+      .expect("Content-Type", /json/)
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+});
