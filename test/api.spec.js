fac3294f4060b372b706317cc905102ad5aae02a
Maruan100 <myousri@jabary.com>
2022-02-07 17:26:07 +0100

    fix: config.yml

--- test/api.spec.js
+++ test/api.spec.js
@@ -5,7 +5,7 @@ const app = require('../src/app.js');
 describe("GET /articles", () => {
   it("should return articles list", (done) => {
     request(app)
-      .get("/articles")
+      .get("/api/articles")
       .expect(200)
       .expect("Content-Type", /json/)
       .end((err) => {
